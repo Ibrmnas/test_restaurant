e6cb20d3242da46490e5afd691fd387688e3c9db
Ibrahim Nasser <106866448+Ibrmnas@users.noreply.github.com>
2025-11-20 20:17:47 +0100

    Delete assets directory

--- assets/js/backend.js
+++
@@ -1,3 +0,0 @@
-// Paste your Apps Script Web App URL between the quotes:
-// window.WB_ENDPOINT = "https://script.google.com/macros/s/AKfycbxpsZfhP3frZ8_gRVtZwXE36lMiF0RHlhOWoqxnxlVUXfQ8-6KDi-22dj86EDWOA_zI/exec"; // e.g., https://script.google.com/macros/s/AKfycbx.../exec
-window.WB_ENDPOINT = "https://script.google.com/macros/s/AKfycbyrgQkYNqdHArejMZ8OJPy9HNznPw96ra4AK6IEaLskZkVFay7SLl-Ho1ttHpGRBu0R/exec"; // e.g., https://script.google.com/macros/s/AKfycbx.../exec
